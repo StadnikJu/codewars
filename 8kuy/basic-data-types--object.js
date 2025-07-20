98201a23b0582389860acf3d17e5af9360a5a19c
StadnikJu <stadnikjulia14@icloud.com>
2025-07-21 00:42:41 +0200

    completed tasks for object

---
+++ 8kuy/basic-data-types--object.js
@@ -0,0 +1,6 @@
+// Give you a function animal, accept 1 parameter:obj like this:
+
+
+function animal(obj){
+  return `this ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
---
+++ 8kuy/sum-arrays.js
@@ -0,0 +1,28 @@
+
+//let arr = [-1, 54, 64, 2, 4];
+
+// function sum (numbers) {
+
+//     if(numbers.length === 0) {
+//         return 0;
+//     } 
+//     let sum = 0;
+//      for(let i = 0; i < numbers.length; i++) {
+//         sum = sum + numbers[i];
+//     }
+    
+//     return sum;
+// }
+
+
+function number(5, -3) {
+
+    let sum = 0;
+    for(let i = 6; i <= 3; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
+
+}
+
