22dd1d6212a60392042c1c99e8af3cd4829adc3d
StadnikJu <stadnikjulia14@icloud.com>
2025-07-28 22:10:05 +0200

    changed code style

--- 8kuy/sort-array-by-string-length.js
+++ 8kuy/sort-array-by-string-length.js
@@ -5,15 +5,14 @@
 
 function sortByLength (array) {
 
-   for(let i = 0; i < array.length; i++) {
-    for(let j = 0; j < array.length-1; j++) {
-        if(array[j].length > array[j + 1].length){
-            let temp = array[j];
-            array[j] = array[j + 1];
-            array[j + 1] = temp;
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < array.length-1; j++) {
+            if(array[j].length > array[j + 1].length){
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
-   }
-
     return array;
 }
