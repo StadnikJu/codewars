a5a296735e92fa86de5a52e7c40303354d37f6d2
StadnikJu <stadnikjulia14@icloud.com>
2026-03-19 14:26:01 +0100

    completed task

---
+++ 7kuy/word-values.js
@@ -0,0 +1,40 @@
+// Given a string "abc" and assuming that each letter 
+// in the string has a value equal to its position in the alphabet,
+// our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ... z = 26.
+// You will be given a list of strings and your task will be to return the values of the strings as explained 
+// above multiplied by the position of that string in the list. For our purpose, position begins with 1.
+// ["abc", "abc abc"] should return [6, 24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+// "abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied 
+// by 1 while the value at position 2 is multiplied by 2.
+// Input will only contain lowercase characters and spaces.
+
+
+function wordValue(words) {
+    let alp = [
+        "a", "b", "c", "d", "e", 
+        "f", "g", "h", "i", "j", 
+        "k", "l", "m", "n", "o", 
+        "p", "q", "r", "s", "t", 
+        "u", "v", "w", "x", "y", "z"
+    ];
+
+    let result = []; 
+
+    for(let i = 0; i < words.length; i++) {
+        let word = words[i]; 
+        let letters = word.split(""); 
+        let sum = 0;
+
+        for (let j = 0; j < letters.length; j++) {
+            let letter = letters[j]; 
+            if(letter !== " ") {
+                let value = alp.indexOf(letter) + 1; 
+                sum += value; 
+            }
+        }
+        
+        result.push(sum * (i + 1)); 
+    }
+
+    return result;  
+}
